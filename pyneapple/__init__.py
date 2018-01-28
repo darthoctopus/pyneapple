2a3e78444efc135e8fa2ebcbce9427355b6a30e3
Joel Ong <joel.ong@yale.edu>
2018-01-28 14:58:06 -0500

    typo

--- pyneapple/__init__.py
+++ pyneapple/__init__.py
@@ -532,7 +532,7 @@ class Pyneapple(Gtk.Application):
         if not self.windows:
             # check to see if we have opened a file recently
             recents = [[q.get_modified(), q.get_uri()] for q in Gtk.RecentManager.get_default().\
-                        get_items() if ('pineapple' in q.get_applications()\
+                        get_items() if ('pyneapple' in q.get_applications()\
                             or 'pyneapple.py' in q.get_applications())]
             if recents:
                 # sort by last visited
