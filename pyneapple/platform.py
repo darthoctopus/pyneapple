93aec9e892cd984d0c853d8d5badc398cfadf4a5
Joel Ong <joel@hyad.es>
2022-07-01 20:53:30 +0100

    strip certain special characters from platformat

--- pyneapple/platform.py
+++ pyneapple/platform.py
@@ -9,7 +9,7 @@ del platform
 if SYSTEM == "Windows":
     # Pylint doesn't like this.
     from gi.repository import WebKit
-    platformat = lambda x: x[2:] if x[1] == ":" else x
+    _platformat = lambda x: x[2:] if x[1] == ":" else x
     config['csd'] = "False"
     class WebView(WebKit.WebView):
         def run_javascript(self, script):
@@ -18,8 +18,10 @@ else:
     gi.require_version('WebKit2', '4.0')
     from gi.repository import WebKit2
     WebView = WebKit2.WebView
-    platformat = lambda x: x
+    _platformat = lambda x: x
 
+def platformat(x):
+    return _platformat(x).replace('%', '%25').replace('?', '%3F')
 
 def open_term(path):
     if SYSTEM == "Windows":
